012ee2bc22ab901f30bbbf17df5f47a97f312684
Sam Clements <sam@borntyping.co.uk>
2018-01-26 11:54:52 +0000

    Stop py.test interfering with tests (fixes #56)
    Can't use py.test log capturing, as we use a custom formatter.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='colorlog',
-    version='3.1.1',
+    version='3.1.2',
 
     description='Log formatting with colors!',
     long_description=open('README.md').read(),
