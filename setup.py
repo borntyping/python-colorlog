151c2aa57c67087653f10cdedbadaad92b3067fc
Sam Clements <sam@borntyping.co.uk>
2018-01-26 11:24:42 +0000

    Release version 3.1.1 (fixes #55)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='colorlog',
-    version='3.1.0',
+    version='3.1.1',
 
     description='Log formatting with colors!',
     long_description=open('README.md').read(),
