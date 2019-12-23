ed2d85cb0e9e175add18230a4720f1e0739c9e59
leroy627 <leroy627@live.com.my>
2019-12-23 08:46:17 +0800

    Wrapped lines to match the rest of the code

--- colorlog/colorlog.py
+++ colorlog/colorlog.py
@@ -87,8 +87,10 @@ class ColoredFormatter(logging.Formatter):
             else:
                 fmt = default_formats['%']
 
-        if sys.version_info > (3, 8) and isinstance(self, LevelFormatter) and isinstance(fmt, dict):
-            super(ColoredFormatter, self).__init__(fmt, datefmt, style, validate=False)
+        if sys.version_info > (3, 8) and isinstance(self, LevelFormatter) \
+                and isinstance(fmt, dict):
+            super(ColoredFormatter, self).__init__(
+                fmt, datefmt, style, validate=False)
         elif sys.version_info > (3, 2):
             super(ColoredFormatter, self).__init__(fmt, datefmt, style)
         elif sys.version_info > (2, 7):
