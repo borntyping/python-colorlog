73b4971e6da80925eb0e9abb7c9eca04b47e5f89
Sam Clements <sam@borntyping.co.uk>
2021-04-13 21:02:20 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="colorlog",
-    version="6.0.0-alpha.1",
+    version="6.0.0-alpha.2",
     description="Add colours to the output of Python's logging module.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
